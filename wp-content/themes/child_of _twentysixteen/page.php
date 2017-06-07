433b0ce992e9db53c986edc417037c575ecbef85
Lauren <lauren_gregory@hotmail.co.uk>
2017-06-07 10:21:56 -0400

    created new header template for the home page and modified sidebar.php to call new header

--- wp-content/themes/child_of _twentysixteen/page.php
+++ wp-content/themes/child_of _twentysixteen/page.php
@@ -1,15 +1,17 @@
 <?php
-
 /**
- *Template Name: no-sidebar.php 
- *The template for displays pages without a sidebar
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
  */
 
-if ( is_front_page() ) :
-	get_header( 'home' );
-else:
-	get_header(); 
-endif; ?>
+get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
@@ -31,6 +33,9 @@ endif; ?>
 
 	</main><!-- .site-main -->
 
+	<?php get_sidebar( 'content-bottom' ); ?>
+
 </div><!-- .content-area -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
